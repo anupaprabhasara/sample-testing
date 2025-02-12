22fbe85f5d3c543419c9709e2d4dafd2a7fb0a89
Anupa Prabhasara <it23556584@my.sliit.lk>
2025-02-12 14:20:06 +0530

    Delete src/main directory

--- src/main/java/com/shop/util/ConnectionData.java
+++
@@ -1,8 +0,0 @@
-package com.shop.util;
-
-public class ConnectionData {
-
-    public static final String DATABASE = "onlineshopping";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "123456";
-}
